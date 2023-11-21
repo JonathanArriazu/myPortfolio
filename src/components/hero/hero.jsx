99a310fd4cf9200f2f3d2e5c364f564817e62323
Jonathan Arriazu <joni.arriazu2@gmail.com>
2023-11-21 05:06:35 -0300

    feat: hero section added

--- src/components/hero/hero.jsx
+++ src/components/hero/hero.jsx
@@ -1,9 +1,36 @@
 import React from 'react';
 import './hero.css';
+import devAnimation from "/public/animation/dev1.json"
+import Lottie from 'lottie-react';
 
 const Hero = () => {
   return (
-    <h1>Hero</h1>
+      <section className='flex hero'>
+
+        <div className='left-section'>
+          <div className="parent-avatar">
+            <img className='avatar' src="./me.jpg" alt="my photo" />
+          </div>
+          <h1 className='title'>
+            Lorem ipsum dolor sit amet consectetur.
+          </h1>
+          <p className='sub-title'>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorem culpa ipsum minus aut possimus accusamus quis voluptatum sit cumque debitis commodi rerum, qui dolorum ducimus maiores deleniti? Ipsa, aliquid illum aut enim laudantium quam, voluptas consectetur, rerum officia ea officiis?
+          </p>
+          <div className='flex all-icons'>
+            <div className="icon icon-github"></div>
+            <div className="icon icon-linkedin"></div>
+          </div>
+        </div>
+
+        <div className='right-section animation'>
+          <Lottie 
+            className='dev-animation'
+            animationData={devAnimation}
+          />
+        </div>
+
+      </section>
   )
 }
 
