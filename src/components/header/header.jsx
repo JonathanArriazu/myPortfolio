fb55a5cc513d3e2ec32f96686a59690bd8cf278c
Jonathan Arriazu <joni.arriazu2@gmail.com>
2023-11-21 03:08:46 -0300

    feat: header and its styles added

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,11 +1,63 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './header.css';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
-  return (
-    <div>
-        <h1>Header</h1>
-    </div>
+
+  const [showModal, setShowModal] = useState(false);
+
+  return (    
+    <header className='flex'>
+      <button 
+        className='flex menu icon-menu' 
+        onClick={() => {setShowModal(true)}}
+      />
+      <nav>
+        <ul className='flex'>
+          <li>
+            <Link to="">About</Link>
+            </li>
+          <li>
+            <Link to="">Articles</Link>
+            </li>
+          <li>
+            <Link to="">Projects</Link>
+            </li>
+          <li>
+            <Link to="">Speaking</Link>
+            </li>
+          <li>
+            <Link to="">Contact</Link>
+            </li>
+        </ul>
+      </nav>
+
+      <button className='flex mode'>
+        <span className='icon-moon-o'></span>
+      </button>
+
+      {showModal && (
+        <div className='border fixed'>
+        <ul className='modal'>
+          <li>
+            <button 
+              className='icon-close'
+              onClick={() => {
+                setShowModal(false)
+              }}
+            />
+          </li>
+          <li><Link to="">About</Link></li>
+          <li><Link to="">Articles</Link></li>
+          <li><Link to="">Projects</Link></li>
+          <li><Link to="">Speaking</Link></li>
+          <li><Link to="">Uses</Link></li>
+        </ul>
+      </div>
+      )}
+
+      
+    </header>
   )
 }
 
