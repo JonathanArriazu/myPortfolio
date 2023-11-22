6558ec14f0b927943a93f8f199969c3986091fee
Jonathan Arriazu <joni.arriazu2@gmail.com>
2023-11-22 17:29:34 -0300

    feat: footer added

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import './footer.css';
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
   return (
-    <h1>
-        Footer
-    </h1>
+    
+    <footer className='flex'>
+
+      <ul className='flex'>
+        <li><Link to="">About</Link></li>
+        <li><Link to="">Projects</Link></li>
+        <li><Link to="">Speaking</Link></li>
+        <li><Link to="">Uses</Link></li>
+      </ul>
+
+      <p>© Jonathan Arriazu. All rights reserved.</p>
+
+    </footer>
+
   )
 }
 
