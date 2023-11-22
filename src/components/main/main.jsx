8750767316e56b0af8f3fe54d13d474158a2f9fd
Jonathan Arriazu <joni.arriazu2@gmail.com>
2023-11-22 17:45:37 -0300

    fix: set active class for selected button on filters buttons

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -1,24 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './main.css';
 import { Link } from 'react-router-dom';
 
 const Main = () => {
+
+  const [currentActive, setCurrentActive] = useState('all');
+  const myProjects = ['aa', 'bb', 'cc']
+
   return (
     <main className='flex'>
 
       <section className='flex left-section'>
-        <button className='active'>All Projects</button>
-        <button>HTML && CSS</button>
-        <button>JavaScript</button>
-        <button>React && MUI</button>
-        <button>Node && Express</button>
+        <button onClick={() => {setCurrentActive('all')}} className={currentActive === 'all' ? 'active' : null}>All Projects</button>
+        <button onClick={() => { setCurrentActive('html-css')}} className={currentActive === 'html-css' ? 'active' : null}>HTML && CSS</button>
+        <button onClick={() => { setCurrentActive('js')}} className={currentActive === 'js' ? 'active' : null}>JavaScript</button>
+        <button onClick={() => { setCurrentActive('react')}} className={currentActive === 'react' ? 'active' : null}>React && MUI</button>
+        <button onClick={() => { setCurrentActive('node-express')}} className={currentActive === 'node-express' ? 'active' : null}>Node && Express</button>
       </section>
 
       <section className='flex right-section'>
 
-        {["aa", "bb", "cc"].map((item) => {
+        {myProjects.map((item) => {
           return(
-            <article className='card'>
+            <article key={item} className='card'>
               <img width={266} src="./js.jpeg" alt="" />
 
               <div style={{width: '266px'}} className='box'>
