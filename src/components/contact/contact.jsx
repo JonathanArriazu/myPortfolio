08e7edfb95c7c679f304fccb6648109a513af894
Jonathan Arriazu <joni.arriazu2@gmail.com>
2023-11-23 01:36:07 -0300

    feat: contact me form added

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -3,7 +3,33 @@ import './contact.css';
 
 const Contact = () => {
   return (
-    <h1>Contact</h1>
+    <section className='contact-me'>
+
+      <h1 className='title'>
+        <span className='icon-envelope'></span>
+        Contact me
+      </h1>
+      <p className='sub-title'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore magni atque perspiciatis at?</p>
+
+      <div className="flex">
+        <form>
+
+          <div className='flex'>
+            <label htmlFor="email">Email Address:</label>
+            <input required type="email" name='' id='email' />
+          </div>
+
+          <div className='flex' style={{marginTop: '24px'}}>
+            <label htmlFor="message">Your message:</label>
+            <textarea required name="" id="message"></textarea>
+          </div>
+
+          <button className='submit'>Submit</button>
+        </form>
+        <div className='border animation'>a</div>
+      </div>
+
+    </section>
   )
 }
 
