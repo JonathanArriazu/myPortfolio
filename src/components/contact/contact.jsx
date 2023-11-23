595127894eed453d6bb0a0874af01fe12f2a324c
Jonathan Arriazu <joni.arriazu2@gmail.com>
2023-11-23 02:29:06 -0300

    feat: email sending functionality added

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import './contact.css';
+import { useForm, ValidationError } from '@formspree/react';
 
 const Contact = () => {
+
+  const [state, handleSubmit] = useForm("xpzggaav");
+  
   return (
     <section className='contact-me'>
 
@@ -12,19 +16,40 @@ const Contact = () => {
       <p className='sub-title'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore magni atque perspiciatis at?</p>
 
       <div className="flex">
-        <form>
+        <form onSubmit={handleSubmit}>
 
           <div className='flex'>
             <label htmlFor="email">Email Address:</label>
-            <input required type="email" name='' id='email' />
+            <input required type="email" name='email' id='email' />
+            <ValidationError 
+              prefix="Email" 
+              field="email"
+              errors={state.errors}
+            />
           </div>
 
           <div className='flex' style={{marginTop: '24px'}}>
             <label htmlFor="message">Your message:</label>
-            <textarea required name="" id="message"></textarea>
+            <textarea required name="message" id="message"></textarea>
+            <ValidationError 
+              prefix="Message" 
+              field="message"
+              errors={state.errors}
+            />
           </div>
 
-          <button className='submit'>Submit</button>
+          <button 
+          className='submit'
+          type="submit" 
+          disabled={state.submitting}
+          >
+            {state.submitting ? "Submitting..." : "Submit"}
+          </button>
+
+          {state.succeeded && (
+            <p style={{fontSize: '18px', marginTop: '1.7rem'}}>Your message has been sent successfully</p>
+            )}
+
         </form>
         <div className='border animation'>a</div>
       </div>
