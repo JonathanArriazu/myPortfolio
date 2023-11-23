69bfcfd50ad35fc10c171419efa536e6df9fb57a
Jonathan Arriazu <joni.arriazu2@gmail.com>
2023-11-22 22:41:42 -0300

    feat: button for filter the data of my projects added

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -1,32 +1,62 @@
 import React, { useState } from 'react';
 import './main.css';
 import { Link } from 'react-router-dom';
+import { myProjects } from './data/myProjects';
 
-const Main = () => {
+const Main = () => {  
 
   const [currentActive, setCurrentActive] = useState('all');
-  const myProjects = ['aa', 'bb', 'cc']
+  const [array, setArray] = useState(myProjects)
+
+  const filterData = (filter) => {
+    setCurrentActive(filter);
+
+    if (filter === "all") {
+      return setArray(myProjects)      
+    }
+
+    const selectedItem = myProjects.filter(item => item.technologies.includes(filter));
+
+    setArray(selectedItem)
+    
+  };
 
   return (
     <main className='flex'>
 
       <section className='flex left-section'>
-        <button onClick={() => {setCurrentActive('all')}} className={currentActive === 'all' ? 'active' : null}>All Projects</button>
-        <button onClick={() => { setCurrentActive('html-css')}} className={currentActive === 'html-css' ? 'active' : null}>HTML && CSS</button>
-        <button onClick={() => { setCurrentActive('js')}} className={currentActive === 'js' ? 'active' : null}>JavaScript</button>
-        <button onClick={() => { setCurrentActive('react')}} className={currentActive === 'react' ? 'active' : null}>React && MUI</button>
-        <button onClick={() => { setCurrentActive('node-express')}} className={currentActive === 'node-express' ? 'active' : null}>Node && Express</button>
+
+        <button onClick={() => {
+          filterData("all");
+        }} className={currentActive === 'all' ? 'active' : null}>All Projects</button>
+        
+        <button onClick={() => {
+          filterData("html-css")  
+        }} className={currentActive === 'html-css' ? 'active' : null}>HTML && CSS</button>
+        
+        <button onClick={() => {
+          filterData("js")
+        }} className={currentActive === 'js' ? 'active' : null}>JavaScript</button>
+        
+        <button onClick={() => {
+          filterData("react");
+        }} className={currentActive === 'react' ? 'active' : null}>React && MUI</button>
+        
+        <button onClick={() => {
+          filterData("node-express")
+        }} className={currentActive === 'node-express' ? 'active' : null}>Node && Express</button>
+      
       </section>
 
       <section className='flex right-section'>
 
-        {myProjects.map((item) => {
+        {array.map((item) => {
           return(
-            <article key={item} className='card'>
-              <img width={266} src="./js.jpeg" alt="" />
+            <article key={item.id} className='card'>
+              <img width={266} src={item.imgPath} alt="" />
 
               <div style={{width: '266px'}} className='box'>
-                <h1 className='title'>Landing Page 2</h1>
+                <h1 className='title'>{item.projectTitle}</h1>
                 <p className='sub-title'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem nesciunt qui deleniti cum illum harum. </p>
                 
                 <div className='flex icons'>
