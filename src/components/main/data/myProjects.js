29bfb8a87abd37c8493aca082d50bad333e5c1e9
Jonathan Arriazu <joni.arriazu2@gmail.com>
2023-11-22 22:39:26 -0300

    feat: myProjects data added

---
+++ src/components/main/data/myProjects.js
@@ -0,0 +1,26 @@
+export const myProjects = [
+    {
+        id: 1, 
+        projectTitle: "React Project", 
+        technologies: ["react"], 
+        imgPath: "./images/react.jpeg"
+    },
+    {
+        id: 2, 
+        projectTitle: "HTML && CSS Project", 
+        technologies: ["html-css", "bootstrap"], 
+        imgPath: "./images//html.jpeg"
+    },
+    {
+        id: 3, 
+        projectTitle: "JAVASCRIPT PROJECT", 
+        technologies: ["html-css", "bootstrap", "js"], 
+        imgPath: "./images/js.jpeg"
+    },
+    {
+        id: 4, 
+        projectTitle: "Node-Express Project", 
+        technologies: ["react", "node-express"], 
+        imgPath: "./images/node.jpeg"
+    }
+]
